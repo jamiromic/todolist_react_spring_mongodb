744b496556bbf3e0fa025867147d3a91ef4e85a0
jamiromic <jamiromic@gmail.com>
2023-10-25 12:08:55 +0200

    add logger in main class

--- backend/src/main/java/com/jamiromic/TodoListApplication.java
+++ backend/src/main/java/com/jamiromic/TodoListApplication.java
@@ -1,13 +1,19 @@
 package com.jamiromic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoListApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(TodoListApplication.class);
+
 	public static void main(String[] args) {
+		logger.debug("L'applicazione sta per essere avviata.");
 		SpringApplication.run(TodoListApplication.class, args);
+		logger.debug("L'applicazione è stata avviata con successo.");
 	}
 
 }
